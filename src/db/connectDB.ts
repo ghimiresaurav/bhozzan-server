1b10bccdf4319a6b506ad51a679c84fd07492c77
Saurav Ghimire <ghimiresaurav09@gmail.com>
2022-08-22 10:33:19 +0545

    server files, database config

---
+++ src/db/connectDB.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+	let uri = process.env.NODE_ENV === "prod" ? process.env.DB_REMOTE : process.env.DB_LOCAL;
+	try {
+		if (!uri) throw new Error("DB URI not found");
+		await mongoose.connect(uri);
+		console.log("DB CONNECTED SUCCESS!");
+	} catch (error) {
+		process.exit(1);
+	}
+};
+
+export default connectDB;
