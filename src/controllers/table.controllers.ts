db88444961234809184068b1940f4429c98222eb
DexterMes <kabinbhandari33@gmail.com>
2022-10-14 10:12:53 +0545

    Add functionality delete table

--- src/controllers/table.controllers.ts
+++ src/controllers/table.controllers.ts
@@ -3,6 +3,7 @@ import { ITable, ITableDTO } from "../Interfaces/ITable";
 import Restaurant from "../Models/Restaurant.model";
 import Table from "../Models/Table.model";
 import errorHandlers from "../utils/error-handlers";
+import isValidObjectId from "../utils/isValidObjectId";
 
 export const addNewTable: RequestHandler = async (req, res) => {
 	try {
@@ -16,6 +17,30 @@ export const addNewTable: RequestHandler = async (req, res) => {
 		await Restaurant.findByIdAndUpdate(restaurantId, { $push: { tables: table._id } });
 
 		return res.json({ message: "Table Added Successfully" });
+	} catch (error) {
+		console.error(error);
+		return res.status(500).send(errorHandlers(error));
+	}
+};
+
+export const deleteTable: RequestHandler = async (req, res) => {
+	try {
+		const { tableId }: { tableId?: string } = req.params;
+		const restaurantId = req.user.restaurant;
+		if (!isValidObjectId(tableId)) return res.status(400).send("Invalid Dish ID");
+
+		const deletedTable = await Table.findOneAndDelete({
+			_id: tableId,
+			restaurant: req.user.restaurant,
+		});
+
+		if (!deletedTable) return res.status(404).send("Table not found");
+
+		await Restaurant.findByIdAndUpdate(restaurantId, {
+			$pull: { tables: tableId },
+		});
+
+		return res.json({ message: "Table Deleted Successfully" });
 	} catch (error) {
 		console.error(error);
 		return res.status(500).send(errorHandlers(error));
--- src/routes/table.route.ts
+++ src/routes/table.route.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { addNewTable } from "../controllers/table.controllers";
+import { addNewTable, deleteTable } from "../controllers/table.controllers";
 import { isAuthenticated, isManager } from "../middlewares/auth.middleware";
 
 const router: Router = Router();
 
 router.post("/add", isAuthenticated, isManager, addNewTable);
+router.delete("/delete/:tableId", isAuthenticated, isManager, deleteTable);
 
 export default router;
